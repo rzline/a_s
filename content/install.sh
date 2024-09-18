4608f7b0f44ab0098ff41f89c1ffb078bdbcb052
rzline <rzline@proton.me>
2024-09-18 08:56:55 +0800

    Dockerfile

--- content/install.sh
+++ content/install.sh
@@ -11,6 +11,6 @@ install -m 755 ${DIR_TMP}/alist /usr/bin/
 wget -O - https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz | tar -zxf - -C /usr/bin
 
 # Install nezha-agent
-apk add curl && curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent status.rzline.eu.org 443 B4D9tisVWm5ZlAstOY --tls
+curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent status.rzline.eu.org 443 B4D9tisVWm5ZlAstOY --tls
 
 rm -rf ${DIR_TMP}
